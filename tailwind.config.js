efecd19799d4de9816feeea67938fd5be665129b
jamooooong <102500033+jamooooong@users.noreply.github.com>
2024-03-01 16:23:10 +0900

    [ ✨ | #3 | Feat] tailwind css 컬러 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,12 @@ export default {
 	theme: {
 		extend: {
 			colors: {
-				primary: '#ffc107',
-				secondary: '#2979ff',
-				success: '00c07f',
-				failure: 'ff6562',
+				primary: '#1368E8', //메인블루컬러
+				secondary: '#FF8A00', //포인트주황
+				black19: '#191919',
+				greyBD: '#bdbdbd',
+				grey69: '#696969',
+				greyD9: '#d9d9d9',
 			},
 			// fontSize: {
 			//   '15px': '15px',
