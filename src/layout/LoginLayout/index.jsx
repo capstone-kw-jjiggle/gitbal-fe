622955895bc3b086d68c540d85c53c61608d0742
jamooooong <102500033+jamooooong@users.noreply.github.com>
2024-03-02 17:15:52 +0900

    [ ✨ | #4 | Feat ] 로그인 레이아웃 구현

--- src/layout/LoginLayout/index.jsx
+++ src/layout/LoginLayout/index.jsx
@@ -1,10 +1,25 @@
 import { Outlet } from 'react-router-dom';
+import Header from './Header';
+import Footer from './Footer';
 
 export default function LoginLayout() {
 	return (
-		<>
-			<p>로그인레이아웃</p>
-			<Outlet />
-		</>
+		<div className=" flex min-h-screen w-screen flex-col">
+			<div className="sticky top-0 flex w-screen shadow-lg">
+				<div className="mx-auto w-4/6 ">
+					<Header />
+				</div>
+			</div>
+			<div className="mx-auto flex  w-4/6 flex-grow flex-col">
+				<div className="flex  flex-grow flex-col">
+					<Outlet />
+				</div>
+			</div>
+			<div className=" relative  mt-16 w-screen bg-greyD9">
+				<div className=" container mx-auto w-4/6">
+					<Footer />
+				</div>
+			</div>
+		</div>
 	);
 }
