7457fc7ab151230e740ec8dc400b984f25a535d7
jamooooong <102500033+jamooooong@users.noreply.github.com>
2024-03-04 17:02:59 +0900

    [ ✨ | #4 | Feat ] 로그인 레이아웃 구현

--- src/layout/LoginLayout/Header/index.jsx
+++ src/layout/LoginLayout/Header/index.jsx
@@ -2,7 +2,7 @@ import logo from '../../../assets/images/logo/logoEN.svg';
 
 export default function Header() {
 	return (
-		<header className=" h-18 flex w-screen flex-row items-center justify-start bg-white ">
+		<header className=" h-18 flex flex-row items-center justify-start bg-white ">
 			<img alt="logo" src={logo} className="flex h-16 w-32" />
 		</header>
 	);
