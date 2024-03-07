8838e4176505e07965a340068615bea34f3bba1e
suwonthugger <ddww1029wwdd@gmail.com>
2024-03-07 11:21:19 +0900

    [ ✨ | #5 | Feat ] 어바웃 페이지 생성/ nav item 객체 생성

---
+++ src/nav-items/index.js
@@ -0,0 +1,27 @@
+const navItems = {
+	id: 'nav',
+	title: 'nav',
+	type: 'group',
+	children: [
+		{
+			id: 'about',
+			title: 'about',
+			type: 'item',
+			url: '/about',
+		},
+		{
+			id: 'schoolRank',
+			title: 'schoolRank',
+			type: 'item',
+			url: '/school-page',
+		},
+		{
+			id: 'regionalRank',
+			title: 'regionalRank',
+			type: 'item',
+			url: '/region-page',
+		},
+	],
+};
+
+export default navItems;
