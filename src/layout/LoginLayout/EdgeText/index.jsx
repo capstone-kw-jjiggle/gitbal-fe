e8baf3c2bb41a529d1aa1eee547ff12d608520c0
jamooooong <102500033+jamooooong@users.noreply.github.com>
2024-03-08 21:31:54 +0900

    [ ✨ | #12 | Feat] 폰트 굵기 추가 및 적용

--- src/layout/LoginLayout/EdgeText/index.jsx
+++ src/layout/LoginLayout/EdgeText/index.jsx
@@ -1,7 +1,7 @@
 export default function EdgeText({ rotate }) {
 	return (
 		<div className="flex w-1/6 items-center justify-center">
-			<p className={`fixed top-80 pb-36 font-bold ${rotate}`}>Gitbal</p>
+			<p className={`font- fixed top-80 pb-36 font-extrabold ${rotate}`}>Gitbal</p>
 		</div>
 	);
 }
