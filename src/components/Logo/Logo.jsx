a26dd088fb625cc42d05b1f49e6ced4c4acb5d99
jamooooong <102500033+jamooooong@users.noreply.github.com>
2024-03-08 21:15:33 +0900

    [ ✨ | #12 | Feat] nav 로고 교체 및 크기 조정

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -3,7 +3,7 @@ import logo from '../../assets/images/logo/logoEN.svg';
 
 export default function Logo() {
 	return (
-		<Link to="/main-page" className="flex h-16 w-32">
+		<Link to="/main-page" className="flex h-16 w-28">
 			<img alt="logo" src={logo} />
 		</Link>
 	);
