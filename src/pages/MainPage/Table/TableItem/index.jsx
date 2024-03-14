369ca23a2c4e64783c4a64c70bb575884f798d8d
suwonthugger <ddww1029wwdd@gmail.com>
2024-03-14 11:13:49 +0900

    [ ✨ | #13 | Feat ] mock 데이터 연결 및 절대경로를 위한 vite.config.js 수정

--- src/pages/MainPage/Table/TableItem/index.jsx
+++ src/pages/MainPage/Table/TableItem/index.jsx
@@ -1,10 +1,10 @@
-export default function TableItem() {
+export default function TableItem({ rank, name, level, score }) {
 	return (
 		<tr className="h-14 border border-greyD9 text-center text-lg hover:bg-gray-100">
-			<td>1</td>
-			<td>suwonthugger</td>
-			<td>🇰🇷</td>
-			<td>95</td>
+			<td>{rank}</td>
+			<td>{name}</td>
+			<td>{level}</td>
+			<td>{score.toLocaleString()}</td>
 		</tr>
 	);
 }
--- src/pages/MainPage/Table/TableItemGroup/index.jsx
+++ src/pages/MainPage/Table/TableItemGroup/index.jsx
@@ -1,12 +1,12 @@
 import TableItem from '../TableItem';
+import { rankList } from '../_data/mock';
 
 export default function TableItemGroup() {
 	return (
 		<tbody>
-			<TableItem />
-			<TableItem />
-			<TableItem />
-			<TableItem />
+			{rankList.map((user) => (
+				<TableItem key={user.rank} rank={user.rank} name={user.userId} level={user.level} score={user.score} />
+			))}
 		</tbody>
 	);
 }
