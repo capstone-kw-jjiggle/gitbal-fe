43605e389442449eb7a51f6d515485e421aa4359
suwonthugger <ddww1029wwdd@gmail.com>
2024-03-14 10:49:58 +0900

    [ ✨ | #13 | Feat ] mock 데이터 생성

---
+++ src/pages/MainPage/Table/_data/mock.js
@@ -0,0 +1,12 @@
+export const rankList = [
+	{ rank: 1, userId: 'suwonthugger', level: 6, score: 77777777777 },
+	{ rank: 2, userId: 'suwonthugger', level: 6, score: 77777777777 },
+	{ rank: 3, userId: 'suwonthugger', level: 6, score: 77777777777 },
+	{ rank: 4, userId: 'suwonthugger', level: 6, score: 77777777777 },
+	{ rank: 5, userId: 'suwonthugger', level: 6, score: 77777777777 },
+	{ rank: 6, userId: 'suwonthugger', level: 6, score: 77777777777 },
+	{ rank: 7, userId: 'suwonthugger', level: 6, score: 77777777777 },
+	{ rank: 8, userId: 'suwonthugger', level: 6, score: 77777777777 },
+	{ rank: 9, userId: 'suwonthugger', level: 6, score: 77777777777 },
+	{ rank: 10, userId: 'suwonthugger', level: 6, score: 77777777777 },
+];
