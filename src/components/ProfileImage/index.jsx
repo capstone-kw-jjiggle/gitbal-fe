af59ee2fe15a2fd939541bb534445f069d414c2f
suwonthugger <ddww1029wwdd@gmail.com>
2024-03-23 02:28:14 +0900

    [ ✨ | #18 | Feat ] 기본 UI 구현 - 컴포넌트 분리 전

---
+++ src/components/ProfileImage/index.jsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+import image from '../../assets/react.svg';
+
+export default function ProfileImage() {
+	return (
+		<Link to="/my-page">
+			<img src={image} className="h-40 w-40 rounded-full bg-black shadow-2xl" />
+		</Link>
+	);
+}
