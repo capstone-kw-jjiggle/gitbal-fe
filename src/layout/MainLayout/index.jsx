6ad5d753f430e4f8454598fa13ebc56c68fb4834
suwonthugger <ddww1029wwdd@gmail.com>
2024-03-25 13:44:17 +0900

    [ ✨ | #26 | Feat ] 메인 레이아웃 width 조정

--- src/layout/MainLayout/index.jsx
+++ src/layout/MainLayout/index.jsx
@@ -7,13 +7,13 @@ export default function MainLayout() {
 	return (
 		<div className=" flex min-h-screen w-screen flex-col">
 			<div className="sticky top-0 flex w-screen shadow-lg">
-				<div className="mx-auto w-4/6 ">
+				<div className="mx-auto w-3/5">
 					<Header />
 				</div>
 			</div>
 			<Outlet />
 			<div className=" relative  mt-16 w-screen bg-greyD9">
-				<div className=" container mx-auto w-4/6">
+				<div className=" container mx-auto w-3/5">
 					<Footer />
 				</div>
 			</div>
