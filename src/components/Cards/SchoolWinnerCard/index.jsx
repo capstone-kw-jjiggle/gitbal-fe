aba33bd03c056172aed76f8cf5745b9b569c34ae
suwonthugger <ddww1029wwdd@gmail.com>
2024-03-27 11:09:39 +0900

    [ ✨ | #29 | Feat ] 학교 랭킹페이지 상단 컴포넌트 분리

---
+++ src/components/Cards/SchoolWinnerCard/index.jsx
@@ -0,0 +1,34 @@
+import levelImage6 from '@/assets/images/userLevel/userLevel6.svg';
+
+export const SchoolWinnerCard = () => {
+	return (
+		<>
+			<div className="mt-8 flex w-full items-center justify-between rounded-2xl border border-gray-200 px-12 py-6 text-black19 shadow-lg">
+				<p className="text-3xl font-extrabold">1위</p>
+				<div className="flex w-1/2 ">
+					<div className="mt-6 flex w-full flex-col">
+						<div className="flex justify-between text-2xl">
+							<p className="font-bold">호서대학교</p>
+							<p>3,131,232점</p>
+						</div>
+						<hr className="my-2 w-full rounded-2xl border-b-4 border-primary" />
+						<div className="flex justify-end">
+							<p>MVP</p>
+							<p className="ml-4 text-primary">suwonthugger</p>
+						</div>
+					</div>
+				</div>
+				<div>
+					<p>학교 점수 상승</p>
+					<p className="text-green-500">+23,112점</p>
+				</div>
+				<div>
+					<img src={levelImage6} className="ml-2 h-20 w-20" />
+				</div>
+			</div>
+			<div className="mb-10 mt-4 flex w-full justify-end text-black19">
+				<p>시즌4 보상 칭호 맛꿀마수호자 </p>
+			</div>
+		</>
+	);
+};
