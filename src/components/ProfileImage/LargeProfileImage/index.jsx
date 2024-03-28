3bb6e5aff3516c4d0bc9aa349e6b9cf74dbc2ab4
suwonthugger <ddww1029wwdd@gmail.com>
2024-03-28 11:28:35 +0900

    [ ✨ | #31 | Feat ] 라지프로필이미지 컴포넌트 쉐도우 변경

--- src/components/ProfileImage/LargeProfileImage/index.jsx
+++ src/components/ProfileImage/LargeProfileImage/index.jsx
@@ -4,7 +4,7 @@ import image from '@/assets/react.svg';
 export const LargeProfileImage = () => {
 	return (
 		<Link to="/my-page">
-			<img src={image} className="h-40 w-40 rounded-full bg-black shadow-2xl" />
+			<img src={image} className="h-40 w-40 rounded-full bg-black shadow-xl" />
 		</Link>
 	);
 };
