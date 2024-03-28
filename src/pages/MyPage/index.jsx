dd7ddb320d36735d703109c81beb9ef513ac33bd
suwonthugger <ddww1029wwdd@gmail.com>
2024-03-28 11:27:56 +0900

    [ ✨ | #31 | Feat ] 게이지바 모두 구현 (컴포넌트 분리 전)

--- src/pages/MyPage/index.jsx
+++ src/pages/MyPage/index.jsx
@@ -10,7 +10,7 @@ export default function MyPage() {
 	return (
 		<>
 			<MainContainer>
-				<div className="mt-16 flex items-center justify-between">
+				<div className="mb-14 mt-16 flex items-center justify-between">
 					<MyProfile />
 					<Shortcut />
 				</div>
@@ -36,6 +36,32 @@ export default function MyPage() {
 
 						<div className="absolute -top-10 right-0">
 							<img src={level2} className="h-8 w-8" />
+						</div>
+					</div>
+					<p className="mt-2 text-end text-sm font-bold text-grey69">NEXT RANK 52/100</p>
+				</div>
+				<div className="mt-8 flex flex-col">
+					<h2 className="text-3xl font-extrabold">나와 함께 달리고 있는 사람들</h2>
+					<div className="relative mt-24 h-10 w-full rounded-lg bg-gray-200">
+						<div className="absolute h-full w-1/3 rounded-lg">
+							<div className="absolute -top-10 right-0">
+								<SmallProfileImage />
+							</div>
+						</div>
+						<div className="absolute h-full w-1/2 rounded-lg bg-primary">
+							<div className="absolute -top-10 right-0">
+								<SmallProfileImage />
+							</div>
+						</div>
+						<div className="absolute h-full w-9/12 rounded-lg ">
+							<div className="absolute -top-10 right-0">
+								<SmallProfileImage />
+							</div>
+						</div>
+						<div className="absolute h-full w-3/12 rounded-lg ">
+							<div className="absolute -top-10 right-0">
+								<SmallProfileImage />
+							</div>
 						</div>
 					</div>
 				</div>
