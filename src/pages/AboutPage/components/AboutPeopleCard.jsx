805c2adae853be7acbf34de6183203507f4b6230
jamooooong <102500033+jamooooong@users.noreply.github.com>
2024-05-15 22:19:59 +0900

    [ ✨ | #49 | Feat] 프로필 카드 여러번 클릭하면 숨겨진 이미지 표시

--- src/pages/AboutPage/components/AboutPeopleCard.jsx
+++ src/pages/AboutPage/components/AboutPeopleCard.jsx
@@ -1,34 +1,52 @@
 import logo from '@/assets/images/userLevel/userLevel3.svg';
 import openInNew from '@/assets/images/icon/open_in_new.svg';
+import { useState } from 'react';
+import testImg2 from '@/assets/images/testImg/aboutTestImg2.jpeg';
 
 export default function AboutPeopleCard({ name, part, nickName, gitId, blaBla }) {
+	const [clickCount, setClickCount] = useState(0);
+
 	const handleOpenGitHubWindow = () => {
 		const url = `https://github.com/${gitId}`;
 		window.open(url, '_blank');
 	};
 
-	return (
-		<>
-			<div className="w-full rounded-xl bg-white p-8 text-black19 shadow-xl">
-				<div className="mb-4 flex flex-row justify-between">
-					<div className=" felx-row flex">
-						<p className="text-xl">{name}</p>
-						<p className="my-auto ml-4  text-sm text-grey69">{part}</p>
-					</div>
+	const handleCardClick = () => {
+		console.log('click');
+		setClickCount((prev) => prev + 1);
+		console.log(clickCount);
+	};
 
-					<img src={logo} className="h-7 w-7" alt="logo" />
+	return (
+		<div className="w-full" onClick={handleCardClick}>
+			{clickCount >= 4 ? (
+				<div className="w-full rounded-xl bg-white p-8 text-black19 shadow-xl">
+					<div className="mb-4 flex flex-row justify-between">
+						<img src={testImg2} alt="testImg" className="mt-20 rounded-xl shadow-xl" />
+					</div>
 				</div>
-				<div className="mb-3 flex flex-row text-sm">
-					<p className=" font-bold text-primary">{nickName}</p>
-					<button onClick={handleOpenGitHubWindow} className=" flex flex-row">
-						<p className=" ml-4 font-normal">{gitId}</p>
-						<img src={openInNew} className="my-auto ml-2 mr-1 h-4 w-4 " alt="logo" />
-						<p className="font-normal">GitHub</p>
-					</button>
+			) : (
+				<div className="w-full rounded-xl bg-white p-8 text-black19 shadow-xl">
+					<div className="mb-4 flex flex-row justify-between">
+						<div className=" felx-row flex">
+							<p className="text-xl">{name}</p>
+							<p className="my-auto ml-4  text-sm text-grey69">{part}</p>
+						</div>
+
+						<img src={logo} className="h-7 w-7" alt="logo" />
+					</div>
+					<div className="mb-3 flex flex-row text-sm">
+						<p className=" font-bold text-primary">{nickName}</p>
+						<button onClick={handleOpenGitHubWindow} className=" flex flex-row">
+							<p className=" ml-4 font-normal">{gitId}</p>
+							<img src={openInNew} className="my-auto ml-2 mr-1 h-4 w-4 " alt="logo" />
+							<p className="font-normal">GitHub</p>
+						</button>
+					</div>
+					<p className="mb-3 text-sm text-grey69">광운대학교 정보융합학부</p>
+					<p className="text-sm text-grey69">{blaBla}</p>
 				</div>
-				<p className="mb-3 text-sm text-grey69">광운대학교 정보융합학부</p>
-				<p className="text-sm text-grey69">{blaBla}</p>
-			</div>
-		</>
+			)}
+		</div>
 	);
 }
