b96b13a7619c86dac132fe7d040edea779fcdb37
jamooooong <102500033+jamooooong@users.noreply.github.com>
2024-05-26 18:47:00 +0900

    [ ✨ | #55 | Feat] 로딩페이지에서 Footer 안보이게 설정

--- src/layout/MainLayout/Footer/index.jsx
+++ src/layout/MainLayout/Footer/index.jsx
@@ -1,21 +1,26 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { routes } from '@/constants/routes';
 
 export default function Footer() {
 	const navigate = useNavigate();
+	const location = useLocation();
 	return (
 		<>
-			<footer className=" my-auto flex h-28 w-full flex-col justify-center text-sm text-grey69">
-				<div className=" w-full">
-					<button className="flex" onClick={() => navigate(routes.privacy)}>
-						개인정보처리방침
-					</button>
-					<button className="flex" onClick={() => navigate(routes.tos)}>
-						서비스 이용약관
-					</button>
-					<p className="flex">Contact | gitbal2019@gmail.com</p>
-				</div>
-			</footer>
+			{location.pathname === '/Loading-page' ? (
+				<></>
+			) : (
+				<footer className=" my-auto flex h-28 w-full flex-col justify-center text-sm text-grey69">
+					<div className=" w-full">
+						<button className="flex" onClick={() => navigate(routes.privacy)}>
+							개인정보처리방침
+						</button>
+						<button className="flex" onClick={() => navigate(routes.tos)}>
+							서비스 이용약관
+						</button>
+						<p className="flex">Contact | gitbal2019@gmail.com</p>
+					</div>
+				</footer>
+			)}
 		</>
 	);
 }
