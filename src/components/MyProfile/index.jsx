bc03a5f3d2aef2aba127b12d8c50e1b4f7b1b3aa
jamooooong <102500033+jamooooong@users.noreply.github.com>
2024-05-26 16:37:28 +0900

    [ ✨ | #55 | Feat] 미 로그인시 표시 되는 문장을 페이지별로 다르게 구현

--- src/components/MyProfile/index.jsx
+++ src/components/MyProfile/index.jsx
@@ -1,10 +1,11 @@
 import { LargeProfileImage } from '@components/ProfileImage';
 import levelImage from '@/assets/images/userLevel/userLevel3.svg';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 export default function MyProfile() {
 	const isLogined = false;
 
+	const location = useLocation();
 	const navigate = useNavigate();
 
 	return (
@@ -25,7 +26,13 @@ export default function MyProfile() {
 			) : (
 				<div className=" mx-auto text-black19">
 					<div className="flex flex-col place-items-center ">
-						<p className=" text-3xl font-extrabold ">우리 학교 점수가 궁금한가요?</p>
+						{location.pathname === '/school-page' ? (
+							<p className=" text-3xl font-extrabold ">우리 학교 점수가 궁금한가요?</p>
+						) : location.pathname === '/region-page' ? (
+							<p className=" text-3xl font-extrabold ">우리 지역 점수가 궁금한가요?</p>
+						) : (
+							<p className=" text-3xl font-extrabold ">나의 코딩 실력이 궁금하신가요?</p>
+						)}
 						<p className="mt-4 text-sm  text-primary">지금 깃허브로 로그인하고 알아보기</p>
 						<button
 							className="mt-4 flex h-10 w-36 place-content-center place-items-center rounded bg-primary text-sm font-semibold text-white hover:bg-blue-400"
